7450a9f3848a945f4002b477854837f074841bba
cm5wxjmcpv-bit <cm5wxjmcpv@privaterelay.appleid.com>
2026-07-29 11:21:36 -0400

    Update Apps Script deployment URL

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,9 @@
 export const APP_CONFIG = {
   appName: "TargetSolutions Dashboard",
   departmentName: "Martinsville Fire & EMS",
-  apiUrl: "https://script.google.com/macros/s/AKfycbz6DjEd3WdowFVDk1fs8YZhHD-f1eR043QRlkkHNHbfHyPDEBFU3AIscICJdBI-YQ6C/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxxlAtOJJZKmV3eB9pXpycVlo26OMXutP8saC0a_-Gbz-chnx8Sx14hodPYzOI84O8r/exec",
   legacyApiUrls: [
+    "https://script.google.com/macros/s/AKfycbz6DjEd3WdowFVDk1fs8YZhHD-f1eR043QRlkkHNHbfHyPDEBFU3AIscICJdBI-YQ6C/exec",
     "https://script.google.com/macros/s/AKfycbytTrK4cqkFuU30-ZK5lqAWpVyjw0cln_1JyQDTnmE55GBt35_thViWr97oSX8Fn51G/exec",
     "https://script.google.com/macros/s/AKfycbwqAP0qVUUElDZydwU8SnOpI0lCX-4qbZjiHSHIfJz5KRm3HvymVfeK435ukF9HgTMD/exec",
     "https://script.google.com/macros/s/AKfycbyqtrFHrVhbwuQ9RSQ5cMSnPRbdygxLX15cSOlFWDJsmpB5ec-_WM1nSe6awCUVHr5M/exec",
