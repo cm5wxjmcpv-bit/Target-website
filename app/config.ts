8f92cc21fcbc5cda92b40a453921cd25203851f8
cm5wxjmcpv-bit <cm5wxjmcpv@privaterelay.appleid.com>
2026-07-29 09:18:15 -0400

    Update frontend Apps Script endpoint

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,9 @@
 export const APP_CONFIG = {
   appName: "TargetSolutions Dashboard",
   departmentName: "Martinsville Fire & EMS",
-  apiUrl: "https://script.google.com/macros/s/AKfycbwqAP0qVUUElDZydwU8SnOpI0lCX-4qbZjiHSHIfJz5KRm3HvymVfeK435ukF9HgTMD/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbytTrK4cqkFuU30-ZK5lqAWpVyjw0cln_1JyQDTnmE55GBt35_thViWr97oSX8Fn51G/exec",
   legacyApiUrls: [
+    "https://script.google.com/macros/s/AKfycbwqAP0qVUUElDZydwU8SnOpI0lCX-4qbZjiHSHIfJz5KRm3HvymVfeK435ukF9HgTMD/exec",
     "https://script.google.com/macros/s/AKfycbyqtrFHrVhbwuQ9RSQ5cMSnPRbdygxLX15cSOlFWDJsmpB5ec-_WM1nSe6awCUVHr5M/exec",
   ],
   sessionStorageKey: "target-dashboard-session",
